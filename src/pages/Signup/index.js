8de8b03cdda921f7f2a1b6995fadbd5e5b1047b6
Yerimah <danielyerimah@yahoo.com>
2022-06-17 20:50:35 +0100

    Setup http request for sign up

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,22 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './style.module.css';
+import store from '../../redux/configureStore';
+// import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Signup = () => {
-  const [error, setError] = useState('');
+  const USER_SIGNUP = 'USER_SIGNUP'
+//   const user = useSelector((el) => el.userReducer);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleSubmit = (e) => {
-    setError('');
-    e.preventdefault();
+    axios({
+        method: 'post',
+        url: 'http://localhost:3000/api/v1/users',
+        data: {
+          name: e.target[0].value,
+          username: e.target[1].value,
+          email: e.target[2].value,
+          password: e.target[3].value,
+          confirmPassword: e.target[4].value 
+        },
+        headers: {
+          'Content-Type': 'application/json',
+  
+        },
+      }).then((res) => store.dispatch(
+        { type: USER_SIGNUP, newUser: res.data },
+      )).catch((error) => {
+        console.log(error);
+      });
+  
   };
 
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
-        <form className={style.card}>
+        <form className={style.card} onSubmit={handleSubmit} >
           <div>Signup</div>
-
-          <p role="alert" className={style.error}>{error}</p>
 
           <label htmlFor="name">
             Name
@@ -43,7 +64,7 @@ const Signup = () => {
             <input type="password" id="password" placeholder="Confirm Password" required />
           </label>
 
-          <button type="submit" className={style.btn} onClick={handleSubmit}>Sign Up</button>
+          <button type="submit" className={style.btn}>Sign Up</button>
         </form>
 
         <small>
