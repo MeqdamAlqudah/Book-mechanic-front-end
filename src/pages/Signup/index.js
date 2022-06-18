3158ad3c1c5e5254ff1e48340cf264675ae785c6
Yerimah <danielyerimah@yahoo.com>
2022-06-18 14:45:18 +0100

    Fix internal server error

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import style from './style.module.css';
-import store from '../../redux/configureStore';
-// import { useSelector } from 'react-redux';
-import axios from 'axios';
+// import store from '../../redux/configureStore';
+// // import { useSelector } from 'react-redux';
+// import axios from 'axios';
 
 const Signup = () => {
-  const USER_SIGNUP = 'USER_SIGNUP'
-//   const user = useSelector((el) => el.userReducer);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const [ name, setName ] = useState('');
+    const [ username, setUsername ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ confirmPassword, setConfrimPassword ] = useState('');
 
-    axios({
-        method: 'post',
-        url: 'http://localhost:3000/api/v1/users',
-        data: {
-          name: e.target[0].value,
-          username: e.target[1].value,
-          email: e.target[2].value,
-          password: e.target[3].value,
-          confirmPassword: e.target[4].value 
-        },
-        headers: {
-          'Content-Type': 'application/json',
-  
-        },
-      }).then((res) => store.dispatch(
-        { type: USER_SIGNUP, newUser: res.data },
-      )).catch((error) => {
-        console.log(error);
-      });
-  
-  };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            name: name,
+            email: email,
+            password: password,
+            password_confirmation: confirmPassword,
+            address: 'Nigeria',
+            phone: 123,
+            profession: 'Developer'
+             
+        }
+        console.log(data)
+
+        try {
+            let res = await fetch('http://localhost:3000/api/v1/users', {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json', }
+            });
+
+            // let resJson = await res.json();
+            // console.log(resJson)
+            
+            if (res.status === 200) {
+                setName('')
+                setUsername('')
+                setEmail('')
+                setPassword('')
+                setConfrimPassword('')
+            } else {
+                return "some error occured"
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
 
   return (
     <div className={style.container}>
@@ -41,27 +62,27 @@ const Signup = () => {
 
           <label htmlFor="name">
             Name
-            <input type="name" id="name" placeholder="Name" required />
+            <input type="name" id="name" placeholder="Name" onChange={(e) => setName(e.target.value)} required />
           </label>
 
           <label htmlFor="username">
             Username
-            <input type="text" id="username" placeholder="Username" required />
+            <input type="text" id="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} required />
           </label>
 
           <label htmlFor="email">
             Email
-            <input type="email" id="eamil" placeholder="Email" required />
+            <input type="email" id="eamil" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
           </label>
 
           <label htmlFor="password">
             Password
-            <input type="password" placeholder="Password" required />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
           </label>
 
           <label htmlFor="confirmPassword">
             Confirm Password
-            <input type="password" id="password" placeholder="Confirm Password" required />
+            <input type="password" id="password" placeholder="Confirm Password" onChange={(e) => setConfrimPassword(e.target.value)} required />
           </label>
 
           <button type="submit" className={style.btn}>Sign Up</button>
@@ -78,3 +99,30 @@ const Signup = () => {
 };
 
 export default Signup;
+
+
+
+//   const USER_SIGNUP = 'USER_SIGNUP'
+// //   const user = useSelector((el) => el.userReducer);
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     axios({
+//         method: 'post',
+//         url: 'http://localhost:3000/api/v1/users',
+//         data: {
+//           name: e.target[0].value,
+//           username: e.target[1].value,
+//           email: e.target[2].value,
+//           password: e.target[3].value,
+//           confirmPassword: e.target[4].value 
+//         },
+//         headers: {
+//           'Content-Type': 'application/json',
+  
+//         },
+//       }).then((res) => store.dispatch(
+//         { type: USER_SIGNUP, newUser: res.data },
+//       )).catch((error) => {
+//         console.log(error);
+//       });
