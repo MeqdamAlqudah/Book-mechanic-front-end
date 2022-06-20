12bedbc5a0bde22b05e47c751bd157381c8c7ada
Yerimah <danielyerimah@yahoo.com>
2022-06-20 14:55:50 +0100

    Modify redux action

--- src/redux/user/user.js
+++ src/redux/user/user.js
@@ -1,6 +1,8 @@
 const initialState = {};
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case USER_LOGIN:
+      return { ...state, current_user: action.newUser };
     default:
       return state;
   }
