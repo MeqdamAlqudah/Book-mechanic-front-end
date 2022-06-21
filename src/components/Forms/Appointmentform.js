43d87bb19b2b4e77a2da36e0e9eba36eb98e78d0
Yerimah <danielyerimah@yahoo.com>
2022-06-21 21:42:37 +0100

    Create appointment form component

---
+++ src/components/Forms/Appointmentform.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Appointmentform = () => {
+    const [city, setCity] = useState();
+    const [date, setDate] = useState();
+    const [openSelect, setOpenSelect] = useState(false);
+        
+
+  return (
+      <>
+    <div>Appointmentform</div>
+    </>
+  )
+};
+
+export default Appointmentform;
