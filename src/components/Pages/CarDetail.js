ead7c7da58dc5a1c8b9d26f28d09cedb989fde29
MeqdamAlqudah <memuq56@gmail.com>
2022-06-21 18:15:59 +0300

    Fix the car detail issue

--- src/components/Pages/CarDetail.js
+++ src/components/Pages/CarDetail.js
@@ -6,12 +6,13 @@ import AxiosWrapper from '../../requirments/AxiosWrapper';
 const CarDetail = ({ userId }) => {
   const [currentCar, setCurrentCar] = useState({});
   const carId = useSelector((el) => el.carDetailReducer.currentCarId || JSON.parse(localStorage.getItem('carId')));
+
   useEffect(() => {
     localStorage.setItem('carId', JSON.stringify(carId));
     AxiosWrapper(`http://127.0.0.1:3000/api/v1/users/${userId}/cars/${carId}`).then((res) => {
       setCurrentCar(res.data);
     });
-  }, []);
+  }, [carId]);
   if (Object.keys(currentCar).length <= 0) {
     return (<div>loading...</div>);
   }
